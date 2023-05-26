695f6e8c7d7fcfab12526a0c811440efdfdfab85
armdupre <a-joseph.dupre@keysight.com>
2023-05-26 13:46:12 -0400

    Updated.

--- output.tf
+++ output.tf
@@ -13,7 +13,6 @@ output "PrivateSubnet" {
 	value = {
 		"availability_zone" : aws_subnet.PrivateSubnet.availability_zone
 		"id" : aws_subnet.PrivateSubnet.id
-		"name" : aws_subnet.PrivateSubnet.name
 	}
 }
 
@@ -28,7 +27,6 @@ output "PublicSubnet" {
 	value = {
 		"availability_zone" : aws_subnet.PublicSubnet.availability_zone
 		"id" : aws_subnet.PublicSubnet.id
-		"name" : aws_subnet.PublicSubnet
 	}
 }
 
@@ -40,7 +38,6 @@ output "Vpc" {
 		"enable_dns_support" : aws_vpc.Vpc.enable_dns_support
 		"id" : aws_vpc.Vpc.id
 		"instance_tenancy" : aws_vpc.Vpc.instance_tenancy
-		"name" : aws.Vpc.name
 		"tags" : aws_vpc.Vpc.tags
 	}
 }
